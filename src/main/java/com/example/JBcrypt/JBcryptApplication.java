617493fa985fa57f40255e9249612664c4d0d147
hashem98 <hashemsmadi98@gmail.com>
2022-03-27 11:39:17 +0300

    initial

---
+++ src/main/java/com/example/JBcrypt/JBcryptApplication.java
@@ -0,0 +1,13 @@
+package com.example.JBcrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JBcryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JBcryptApplication.class, args);
+	}
+
+}
